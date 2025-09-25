44a93adbfcfa78f8d0100895f4e5501d31fa8dea
sunyasheng <suny0a@KL-26595.local>
2025-09-25 10:34:32 +0300

    up

---
+++ thirdparty/paper2figure-dataset/arxiv_tools/experiments/start_download.sh
@@ -0,0 +1,6 @@
+python3 thirdparty/paper2figure-dataset/arxiv_tools/core/filter_metadata.py \
+  --snapshot ./arxiv-metadata-oai-snapshot.json \
+  --categories cs.CV,cs.LG \
+  --year 2020 \
+  --limit 200 \
+  --output thirdparty/paper2figure-dataset/arxiv_tools/output
